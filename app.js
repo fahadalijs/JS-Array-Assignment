ffa5dd52e5662a6a26886e85b11088e7b467d2a2
fahadalijs <fahad9827022@gmail.com>
2024-03-12 09:28:30 +0500

    Array-project-Complete

---
+++ app.js
@@ -0,0 +1,128 @@
+// const shop = ['Apple', 'Banana',  'Orange', 'Mango', 'Grape', 'Strawberry'];
+// const input = document.querySelector("#input");
+// const h2 = document.querySelector("h2")
+// 
+// function getfruit(){
+    // console.log(input.value);
+// 
+    // if(shop.includes(input.value)){
+        // console.log("fruit avaliable ha....");
+        // h2.innerHTML = `${input.value} is avalible` 
+    // }else{
+        // console.log("fruit avaliable nahi ha...");
+        // h2.innerHTML = `${input.value} is not avalible` 
+// 
+    // }
+    // input.value = ""
+// }
+// 
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const shop = ['Apple', 'Banana',  'Orange', 'Mango', 'Grape', 'Strawberry'];
+// const input = document.querySelector("#input");
+// const h2 = document.querySelector("h2");
+
+// function getfruit(){
+//     console.log(input.value);
+
+//     if(shop.includes(input.value)){
+//         console.log("fruit avaliable ha....");
+//         h2.innerHTML = `${input.value} is avaliable`
+//     }else{
+//         console.log("fruit avaliable nhi ha....");
+//         h2.innerHTML = `${input.value} is not avaliable`
+//  }
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const shop = ['Apple', 'Banana',  'Orange', 'Mango', 'Grape', 'Strawberry'];
+// const input = document.querySelector("#input");
+// const h2 = document.querySelector("h2")
+
+// function getfruit(){
+//     console.log(input.value);
+
+
+//     if(shop.includes(input.value)){
+//         console.log("Fruit is avaliable");
+//         h2.innerHTML = `${input.value} is avliable`
+
+//     }else{
+//         console.log("Fruit is not avaliable");
+//         h2.innerHTML = `${input.value} is not avliable`
+//      }
+//      input.value = ''
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
